7d5041c3b5805fcccab3140a2d05e365745e5c65
nimairoy <nimairoy718@gmail.com>
2023-04-30 14:30:12 +0600

    environment setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log(import.meta.env.VITE_APIKEY)
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDzWk5YJuz4leJr9ESWhGARm0-Xf8fju8I",
-  authDomain: "the-dragon-news-b067c.firebaseapp.com",
-  projectId: "the-dragon-news-b067c",
-  storageBucket: "the-dragon-news-b067c.appspot.com",
-  messagingSenderId: "6000696509",
-  appId: "1:6000696509:web:170899b542a90c7da16bd4"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
